603999f058875c99e15a9cd412e20d66676c409a
VivekSabbani <sabbanivivek@gmail.com>
2022-03-23 12:59:28 +0530

    add user feedback for sent message and location

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,17 @@
 const socket = io();
-
-document.querySelector('#message-form').addEventListener('submit',(event)=>{
+$messageForm = document.querySelector('#message-form');
+$messageFormInput = document.querySelector('#message-input');
+$messageSubmitButton = document.querySelector('#message-send');
+$sendLocationButton = document.querySelector('#send-location');
+$messageForm.addEventListener('submit',(event)=>{
     event.preventDefault();
     // const message = document.querySelector('#user-input').value;
+    $messageSubmitButton.setAttribute('disabled','disabled');
     const message = event.target.elements.userInput.value;
     socket.emit('message',message,(error)=>{
+        $messageSubmitButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if(error){
            return console.log(error);
         }
@@ -16,13 +23,15 @@ socket.on('message',(message)=>{
     console.log(message);
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
+    $sendLocationButton.setAttribute('disabled','disabled');
   if(!navigator.geolocation){
       return alert("Your browser doesn't support location sharing!!!");
   }
   navigator.geolocation.getCurrentPosition((position)=>{
       const {latitude, longitude} = position.coords;
       socket.emit('sendLocation',{latitude,longitude},()=>{
+          $sendLocationButton.removeAttribute('disabled');
           console.log("Location shared!!!");
       });
   })
